a57f28bfd7a0a1dd95d1b201c0a754bf8d75b1db
MarskyMessier <marskymessier@gmail.com>
2025-10-12 03:22:51 +0200

    fix: stray div + spelling & responsive

--- resources/views/widgets/_dashboard_guide.blade.php
+++ resources/views/widgets/_dashboard_guide.blade.php
@@ -8,13 +8,12 @@
             {!! $guide->parsed_text !!}
         </div>
         <div class="card-footer">
-        <div class="d-flex flex-row align-items-center justify-content-between">
-            <span class="text-right"><b>You can hide this pop-up by clicking the button at the right. If you miss it, you can change its visibility back in <a href="{{ url('account/settings')}}">your settings</a>.</b></span>
+        <div class="d-flex flex-sm-row flex-column align-items-center justify-content-between">
+            <span class="text-center text-sm-right mx-4 my-2"><b>You can hide this pop-up by clicking the button at the right. If you miss it, you can change its visibility back in <a href="{{ url('account/settings')}}">your settings</a>.</b></span>
             {!! Form::open(['url' => 'account/guideHome', 'class' => 'text-right']) !!}
-            {!! Form::submit('I understand - Dot not show', ['class' => 'btn btn-primary', 'name' => 'action']) !!}
+            {!! Form::submit('I understand - Do not show', ['class' => 'btn btn-primary', 'name' => 'action']) !!}
             {!! Form::close() !!}
         </div>
-    </div>
         </div>
     </div>
 
