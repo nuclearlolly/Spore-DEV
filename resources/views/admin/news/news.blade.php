905deef02dab29e18efe3c2c9616dec49a86a050
Speedy <speedydvv@gmail.com>
2026-01-26 16:16:41 +0100

    feat(news): add sort and search functions to public, admin indices (#1417)
    * Qol(News): Sort and search functions
    
    * Forgot Request $request AND left a chunk of code in by accident

--- resources/views/admin/news/news.blade.php
+++ resources/views/admin/news/news.blade.php
@@ -12,6 +12,33 @@
     <p>You can create new news posts here. Creating a news post alerts every user that there is a new post, unless the post is marked as not viewable (see the post creation page for details).</p>
 
     <div class="text-right mb-3"><a class="btn btn-primary" href="{{ url('admin/news/create') }}"><i class="fas fa-plus"></i> Create New Post</a></div>
+
+    <div>
+        {!! Form::open(['method' => 'GET', 'class' => 'form-inline justify-content-end mb-3']) !!}
+        <div class="form-group">
+            {!! Form::text('title', Request::get('title'), ['class' => 'form-control', 'placeholder' => 'Name']) !!}
+        </div>
+        <div class="form-group ml-3">
+            {!! Form::select(
+                'sort',
+                [
+                    'bump-reverse' => 'Updated Newest',
+                    'bump' => 'Updated Oldest',
+                    'newest' => 'Created Newest',
+                    'oldest' => 'Created Oldest',
+                    'alpha' => 'Sort Alphabetically (A-Z)',
+                    'alpha-reverse' => 'Sort Alphabetically (Z-A)',
+                ],
+                Request::get('sort') ?: 'Updated Newest',
+                ['class' => 'form-control'],
+            ) !!}
+        </div>
+        <div class="form-group ml-3">
+            {!! Form::submit('Search', ['class' => 'btn btn-primary']) !!}
+        </div>
+        {!! Form::close() !!}
+    </div>
+
     @if (!count($newses))
         <p>No news found.</p>
     @else
