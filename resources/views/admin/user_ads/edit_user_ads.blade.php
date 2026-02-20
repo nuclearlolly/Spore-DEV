2127dc6e77a118c87e216dbede3abd352f0f994a
Camyza <Camyza@users.noreply.github.com>
2026-02-20 09:29:36 +0000

    refactor: fix blade formatting

--- resources/views/admin/user_ads/edit_user_ads.blade.php
+++ resources/views/admin/user_ads/edit_user_ads.blade.php
@@ -33,11 +33,10 @@
     @parent
     <script>
         $(document).ready(function() {
-			$('.delete-user-ad-button').on('click', function(e) {
-            e.preventDefault();
-            var id = $(this).data('id');
-            loadModal("{{ url('admin/user_ads/delete') }}/" + id, 'Delete Ad');
-        });
-		
+                    $('.delete-user-ad-button').on('click', function(e) {
+                        e.preventDefault();
+                        var id = $(this).data('id');
+                        loadModal("{{ url('admin/user_ads/delete') }}/" + id, 'Delete Ad');
+                    });
     </script>
 @endsection
